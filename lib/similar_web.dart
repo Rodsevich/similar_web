25122b1ccd32b1b3e9741aef5223b55185dfb1ec
Rodsevich <nicorodsevich@gmail.com>
2023-09-18 10:23:53 -0300

    issues removal

--- lib/similar_web.dart
+++ lib/similar_web.dart
@@ -1,4 +1,5 @@
-/// An API consumer for "Similar Web" web statistics service. Use this if you want to know estimated data for web traffic of non-owned pages
+/// An API consumer for "Similar Web" web statistics service. Use this if you
+/// want to know estimated data for web traffic of non-owned pages
 library similar_web;
 
 export 'src/similar_web.dart';
--- test/src/similar_web_test.dart
+++ test/src/similar_web_test.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:test/test.dart';
 import 'package:similar_web/similar_web.dart';
+import 'package:test/test.dart';
 
 void main() {
   group('SimilarWeb', () {
